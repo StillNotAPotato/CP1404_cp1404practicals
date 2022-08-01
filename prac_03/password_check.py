0bf3fce92527196a718f5751e62014b7b15d6901
NotAPotato2406 <t.b.siang@outlook.com>
2022-08-01 13:25:11 +0800

    Refactor password checker, use functions now

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,7 +1,7 @@
 MIN_LENGTH = 8
 
 
-def main():
+def version_1():
     """
     Ask user to enter password, minimum 8 characters. Then print in asterisks.
     """
@@ -13,4 +13,25 @@ def main():
         print("*" * len(password))
 
 
-main()
+# version_1()
+
+
+def main():
+    """Ask user to enter password, minimum 8 characters. Then print in asterisks."""
+    password = get_password()
+    encode_password(password)
+
+
+def encode_password(password):
+    """Takes the length of password and print in asterisks"""
+    if len(password) >= 8:
+        print("*" * len(password))
+
+
+def get_password():
+    """Takes password and checks if it meets minimum length requirement"""
+    password = input(f"Please enter a password of minimum {MIN_LENGTH} characters: ")
+    while len(password) < 8:
+        print("Password does not meet minimum length requirement")
+        password = input(f"Please enter a password of minimum {MIN_LENGTH} characters: ")
+    return password
