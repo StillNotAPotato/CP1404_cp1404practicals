af5e549cb8c7dcd706aa994686fca36068965696
NotAPotato2406 <t.b.siang@outlook.com>
2022-08-01 12:50:51 +0800

    Initial commit.

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,5 +1,8 @@
 def main():
-    pass
+    password = input("Password: ")
+    if 9 < len(password) or len(password) > 17:
+        print("Password length should be 10 to 16 characters.")
+
 
 
 main()
