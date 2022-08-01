7d9df897abc1ca88653d78c82450f136f2c7fe65
NotAPotato2406 <t.b.siang@outlook.com>
2022-08-01 13:22:39 +0800

    Refactor password checking condition

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,8 +1,16 @@
-def main():
-    password = input("Password: ")
-    if 9 < len(password) or len(password) > 17:
-        print("Password length should be 10 to 16 characters.")
+MIN_LENGTH = 8
 
+
+def main():
+    """
+    Ask user to enter password, minimum 8 characters. Then print in asterisks.
+    """
+    password = input(f"Please enter a password of minimum {MIN_LENGTH} characters: ")
+    while len(password) < 8:
+        print("Password does not meet minimum length requirement")
+        password = input(f"Please enter a password of minimum {MIN_LENGTH} characters: ")
+    if len(password) >= 8:
+        print("*" * len(password))
 
 
 main()
