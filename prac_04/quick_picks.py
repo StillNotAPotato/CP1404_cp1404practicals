d25ff9e92f36b190cf1802db904ec258d6ca3c2e
NotAPotato2406 <t.b.siang@outlook.com>
2022-08-08 22:17:24 +0800

    print out number of picks user inputted

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,15 +1,28 @@
+import random
+
 MINIMUM = 1
 MAXIMUM = 45
 NUMBER_OF_DIGITS_IN_LINE = 6
 
 
 def main():
-    number_of_picks = int(input("How many picks do you want: "))
+    global quick_pick
+    number_of_picks = int(input("How many quick picks? "))
     while number_of_picks <= 0:
         print("Got to be at least one mate")
-        number_of_picks = int(input("How many picks do you want: "))
+        number_of_picks = int(input("How many quick picks? "))
 
+    for i in range(number_of_picks):
+        quick_pick = []     # list to store the digits for the pick
+        for j in range(NUMBER_OF_DIGITS_IN_LINE):
+            number = random.randint(MINIMUM, MAXIMUM)     # for every iteration, number is assigned any value from
+            # 1 to 45
+            while number in quick_pick:
+                number = random.randint(MINIMUM, MAXIMUM)
+                quick_pick.append(number)   # add to the quick pick list
+            quick_pick.sort()   # sort the numbers in ascending order
 
+            print(f"{number}" for number in quick_pick)
 
 
 main()
