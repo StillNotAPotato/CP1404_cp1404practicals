af1b7e5e4b9d2e92266ee764038364605b15c4b9
NotAPotato2406 <t.b.siang@outlook.com>
2022-08-08 13:20:10 +0800

    Added username checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -8,8 +8,16 @@ def main():
     print("The last number is", numbers[4])
     print("The smallest number is", min(numbers))
     print("The largest number is", max(numbers))  # max() is to find the largest number in the arg passed into it
-    print("The average of the number is", sum(numbers) / len(numbers))    # calculate sum of all numbers in the
+    print("The average of the number is", sum(numbers) / len(numbers))  # calculate sum of all numbers in the
     # list then divide by its length
 
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConcole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
 
 main()
