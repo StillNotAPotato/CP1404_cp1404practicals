982c0f19945354646febabd7ba3bb957b610fc47
NotAPotato2406 <t.b.siang@outlook.com>
2022-08-17 13:26:17 +0800

    initial commit

--- prac_05/word_occurence.py
+++ prac_05/word_occurence.py
@@ -1,20 +1,19 @@
 def main():
-    pass
-    text = input("Text: ")
     text_to_analyse = {}
+    text = input("Text: ")
     # Text: this is a collection of words of nice words this is a fun thing it is
-    word = text.split()
-    for word in text:     #
-        # print(text)
-        count = text_to_analyse.get(text, 0)
-        text_to_analyse[text] = count + 1
+    words = text.split()
+    for word in words:
+        count = text_to_analyse.get(word, 0)
+        text_to_analyse[word] = count + 1
 
-    text = list(text_to_analyse.keys())
-    text.sort()
+    # display words and frequencies in alphabetical order
+    words = list(text_to_analyse.keys())
+    words.sort()
 
-    for text in text_to_analyse:
-        maximum_length = max(len(word) )
-        print("{{}} : {}".format(text, maximum_length, text_to_analyse[text]))
+    maximum_length = max(len(word) for word in words)
+    for word in words:
+        print("{:{}} : {}".format(word, maximum_length, text_to_analyse[word]))
 
 if __name__ == '__main__':
     main()
