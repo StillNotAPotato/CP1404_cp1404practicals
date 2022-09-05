a57cebb3e88510372584d948dc70325eafae01a2
BSTan <bengsiang.tan@my.jcu.edu.au>
2022-09-05 23:02:19 +0800

    Added function to convert miles to km from text widget

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -7,28 +7,41 @@ MILES_TO_KM = 1.60934
 
 
 class ConvertMilesKm(App):
-    "App to convert miles to km"
-    message = StringProperty()
+    "App to convert miles to km."
+    output_km = StringProperty()
     def build(self):
+        """Build app from kv file."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_update(self):
+    def handle_update(self, miles):
         """Update output label"""
-        result = self.handle_calculation()
-        self.root.ids.output_label.text = str(result)
-    def handle_calculation(self):
-        """Convert miles to km"""
-        value = float(self.root.ids.input_label.text)
-        result = value * MILES_TO_KM
-        return result
-        
+        print("update")
+        self.output_km = str(miles * MILES_TO_KM)
+        #result = self.handle_calculation()
+        #self.root.ids.output_label.text = str(result)
 
-    def handle_increment(self):
-        pass
+    def handle_calculation(self, text):
+        """Convert miles to km."""
+        miles = self.convert_to_number(text)
+        self.handle_update(miles)
 
-    def handle_decrement(self):
-        pass
+
+    def handle_increment(self, text, change):
+        """Update text input if user press up/down."""
+        print("handle increment")
+        miles = self.convert_to_number(text) + change
+        self.root.ids.input_miles.text = str(miles)
+
+    @staticmethod
+    def convert_to_number(text):
+        """Convert text to float."""
+        try:
+            return float(text)
+        except ValueError:
+            return 0.0
+
+
 
 ConvertMilesKm().run()
